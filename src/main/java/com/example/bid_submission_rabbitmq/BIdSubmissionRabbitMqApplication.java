b88dac943783ae302d1e53f718300c1f8ebd1368
Akmal MIngyasharov <mr.akmal4555@mail.ru>
2022-10-17 09:05:48 +0500

    initial commit

---
+++ src/main/java/com/example/bid_submission_rabbitmq/BIdSubmissionRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.bid_submission_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BIdSubmissionRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BIdSubmissionRabbitMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bid_submission_rabbitmq/BIdSubmissionRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bid_submission_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BIdSubmissionRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
